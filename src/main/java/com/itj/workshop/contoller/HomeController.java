97aef7dfbec1468ab45f3fedcb19ea41cef2c10f
Miguel Angel Sanchez Solis <ing.miguels@gmail.com>
2021-11-19 12:51:01 -0600

    Code with errors

--- src/main/java/com/itj/workshop/contoller/HomeController.java
+++ src/main/java/com/itj/workshop/contoller/HomeController.java
@@ -1,2 +1,13 @@
-package com.itj.workshop.contoller;public class HomeController {
+package com.itj.workshop.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String helloWord(){
+        return "Hello world"
+    }
 }
